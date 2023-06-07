1a6e4bf3ecaddfb71572387da145ed61211d8ff0
leokim <kunga077@gmail.com>
2023-06-07 23:57:27 +0900

    feat: feign client name 수정

--- src/main/java/com/wooteco/member/business/client/MailOpenFeign.java
+++ src/main/java/com/wooteco/member/business/client/MailOpenFeign.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "MailOpenFeign", url = "${mail.server.url}")
+@FeignClient(name = "toy-toy-mail", url = "${mail.server.url}")
 public interface MailOpenFeign {
 
     @PostMapping("/mail/send")
