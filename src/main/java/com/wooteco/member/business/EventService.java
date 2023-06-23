f3682f9916532338bd69befc761455e05bc516c1
sungsan <kunga077@gmail.com>
2023-06-24 01:45:41 +0900

    feat: 팔로잉 이벤트 로직 생성

--- src/main/java/com/wooteco/member/business/EventService.java
+++ src/main/java/com/wooteco/member/business/EventService.java
@@ -1,10 +1,16 @@
 package com.wooteco.member.business;
 
+import com.wooteco.member.business.dto.FollowCreateEvent;
 import com.wooteco.member.business.dto.MemberCreateEvent;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +21,10 @@ public class EventService {
     @TransactionalEventListener
     public void listenCreateMemberEvent(MemberCreateEvent event) {
         kafkaTemplate.send("sign-up", event.id(), event.email());
+    }
+
+    @TransactionalEventListener
+    public void listenCreateFollowEvent(FollowCreateEvent event) {
+        kafkaTemplate.send("follow", event.followingId(), String.valueOf(event.followerId()));
     }
 }
